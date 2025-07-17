b525035749f75f3603bc224aa1ce576d671115d8
Gwen <70701439+davithebee@users.noreply.github.com>
2025-07-17 15:29:00 +0200

    Update select-po-tlumaczenie-zatwierdzone.user.js

--- select-po-tlumaczenie-zatwierdzone.user.js
+++ select-po-tlumaczenie-zatwierdzone.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Ctrl/Cmd + O – Ustawienie opcji "Tłumaczenie zatwierdzone"
+// @name         Ctrl/Cmd + O – Ustawienie "Tłumaczenie zatwierdzone"
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      2.1
 // @description  Ustawia opcję "Tłumaczenie zatwierdzone" w select albo wykonuje domyślną akcję Ctrl/Cmd+O
 // @author       Bethink
 // @match        *://*/*
@@ -20,10 +20,10 @@
 
       e.preventDefault(); // zatrzymujemy domyślne zachowanie (otwieranie pliku/linku)
 
-      const selectName = 'slide_content_editorial_stage_en';
+      var selectName = 'slide_content_editorial_stage_en';
       const desiredValue = 'translation_approved';
 
-      const select = document.querySelector(`select[name="${selectName}"]`);
+      var select = document.querySelector(`select[name="${selectName}"]`);
       if (select) {
         console.log(`Znaleziono select o nazwie: ${selectName}`);
         const option = Array.from(select.options).find(opt => opt.value === desiredValue);
@@ -35,9 +35,21 @@
           console.log(`Nie znaleziono opcji "${desiredValue}" w ${selectName}`);
         }
       } else {
-        console.log(`Nie znaleziono selecta "${selectName}" – wykonanie domyślnej akcji otwierania`);
-        // Przywrócenie domyślnego działania skrótu
-        window.location.href = 'about:blank'; // możesz zmodyfikować lub usunąć jeśli nie chcesz nic robić
+        selectName = 'editorial_stage_en';
+        select = document.querySelector(`select[name="${selectName}"]`);
+        if (select) {
+          console.log(`Znaleziono select o nazwie: ${selectName}`);
+          const option = Array.from(select.options).find(opt => opt.value === desiredValue);
+            if (option) {
+                select.value = desiredValue;
+                select.dispatchEvent(new Event('change', { bubbles: true }));
+                console.log(`Ustawiono wartość "${desiredValue}" dla ${selectName}`);
+            } else {
+                console.log(`Nie znaleziono opcji "${desiredValue}" w ${selectName}`);
+            }
+        } else {
+            console.log(`Nie znaleziono selecta "${selectName}" – wykonanie domyślnej akcji otwierania`);
+        }
       }
     }
   });
